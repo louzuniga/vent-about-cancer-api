56a4de93f3ee3c93557018284ec561834227acd0
Lou Zuniga <louzuniga86@gmail.com>
2019-05-12 08:05:34 -0700

    added travis

--- config/db.js
+++ config/db.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
 
+mongoose.Promise = global.Promise;
+
 // Connect to mongoDB using async/await
 const connectDB = async () => {
   try {
