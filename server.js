af5b94fa0d7a8d1bf96068bd303091eb831c3a3f
Lou Zuniga <louzuniga86@gmail.com>
2019-05-21 23:53:36 -0700

    trying to connect to client

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const CLIENT_ORIGIN = require('./config/db');
 const cors = require('cors');
 
 const app = express();
@@ -13,15 +14,12 @@ connectDB();
 // req.body middleware
 app.use(express.json({ extended: false }));
 
-// test API
-app.get('/', (req, res) => res.send('API is still running'));
-
 // Define routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/vents', require('./routes/api/vents'));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
