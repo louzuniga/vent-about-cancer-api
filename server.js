1940ac1388db2190e5fbd49b67965cff8ed44a20
Lou Zuniga <louzuniga86@gmail.com>
2019-06-02 20:21:37 -0700

    heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const connectDB = require('./config/db');
 const CLIENT_ORIGIN = require('./config/db');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 
