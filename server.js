69ef08d18f22bd927b5dc354086f7653bd947644
Lou Zuniga <louzuniga86@gmail.com>
2019-06-13 16:51:41 -0700

    modified client origin

--- server.js
+++ server.js
@@ -21,6 +21,6 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/vents', require('./routes/api/vents'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.CLIENT_ORIGIN || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
