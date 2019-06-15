6ce9538f3ed19deb3c3c30484deef81ddc8b7051
Lou Zuniga <louzuniga86@gmail.com>
2019-06-14 22:31:07 -0700

    fixed routes

--- config/db.js
+++ config/db.js
@@ -20,6 +20,7 @@ const connectDB = async () => {
   }
 };
 
-exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:5000';
+module.exports.CLIENT_ORIGIN =
+  process.env.CLIENT_ORIGIN || 'http://localhost:5000';
 
 module.exports = connectDB;
